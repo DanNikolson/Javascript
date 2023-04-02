fbaef87e8585857b6c81b4da1fb0ddc5fa412296
Dan Nikolson <Kuznetsov556677@yandex.ru>
2023-04-02 11:55:26 +0300

    Homework_1

---
+++ Homework1.js
@@ -0,0 +1,6 @@
+let SecondinMinute = 60;
+let MinutesInHour = SecondinMinute;
+let HoursInDay = 24;
+let DaysInaYear = 365;
+let My_age_in_seconds = Number(prompt("Введите ваш возраст: "));
+console.log("Ваш возраст в секундах: ",My_age_in_seconds*SecondinMinute*MinutesInHour*HoursInDay*DaysInaYear);
---
+++ Homework3.js
@@ -0,0 +1,7 @@
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+let cipher = codeWord1.substring(1,2)+codeWord2.substring(1,2)+codeWord3.substring(1,2)+codeWord4.substring(1,2)+codeWord5.substring(1,2);
+console.log(cipher);
